7d658068b15dfb1fc2d65ccc974cfa1635fdb1b5
이지은 <tori@ryanlab.kr>
2022-04-15 13:05:20 +0900

    navigation.js - add naviStyle

--- frontend/src/components/navigation.js
+++ frontend/src/components/navigation.js
@@ -2,14 +2,20 @@ import { Navbar, Container, Nav, NavLink } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 const Navigation = () => {
+    const naviStyle = {
+        color: '#eee',
+        textDecoration: 'none',
+        margin: '0 10'
+    }
+
     return (
         <Navbar bg="dark" variant="dark">
             <Container>
                 <Navbar.Brand href="/">ExerciseTracker</Navbar.Brand>
                 <Nav className="me-auto">
-                    <Link to='/'>Exercises</Link>
-                    <Link to="/create">Create</Link>
-                    <Link to="/register">Register</Link>
+                    <Link to='/' style={naviStyle}>Exercises</Link>
+                    <Link to="/create" style={naviStyle}>Create</Link>
+                    <Link to="/register" style={naviStyle}>Register</Link>
                 </Nav>
             </Container>
         </Navbar>
